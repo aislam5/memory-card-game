d2d8554368c33ab28436ef4f322877248e0c35a9
aislam5 <abdullahislam900@gmail.com>
2024-11-29 09:02:34 -0500

    Inital Pllaning for Memory Card Game

--- Main.java
+++ Main.java
@@ -1,1 +1,38 @@
+public class Main{
+    public static void main(String[] args){
+        //Project Design
+        /*
+         * Home Screen will have the option to Start Playing Shuffling the Cards 
+         * Using Print Statements the CArs will be Displayed if they are Turned Over they will be Displayed as Turned Over0
+         * --------------
+         * |/\/\/\/\/\/\| 
+         * |\/\/\/\/\/\/|           
+         * |/\/\/\/\/\/\|          
+         * |\/\/\/\/\/\/|           
+         * |/\/\/\/\/\/\|          
+         * |\/\/\/\/\/\/|            
+         * |/\/\/\/\/\/\|           
+         * |\/\/\/\/\/\/|           
+         * -------------
+         * 
+         * 
+         * There will be 10 pairs of Cards that will be present 
+         * Once All Cards are turned over ypu win and the time gets Jotted Down 
+         * If you beat the HIghscore time new HighScore
+         * Cards will be selected to turn over using either numbers of the specific card or if I decide to add graphics then it would
+         * be a completely different story.
+         * 
+         */
 
+         System.out.println("--------------");
+         System.out.println("|<><><><><><>|"); 
+         System.out.println("|<><><><><><>|");       
+         System.out.println("|<><><><><><>|");        
+         System.out.println("|<><><><><><>|");       
+         System.out.println("|<><><><><><>|");       
+         System.out.println("|<><><><><><>|");    
+         System.out.println("|<><><><><><>|");          
+         System.out.println("|<><><><><><>|"); 
+         System.out.println("--------------");        
+    }
+}
